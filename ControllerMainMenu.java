e29308fe847a767d6d49d001f1c1c59ad8f51cb1
Sonal <57592260+amorfati-s@users.noreply.github.com>
2021-03-29 16:49:51 -0400

    files modified

---
+++ ControllerMainMenu.java
@@ -0,0 +1,29 @@
+package RUCafeApp;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
+
+/**
+ * This class processes the GUI from the payroll_processing_gui.fxml in order to
+ * manage different operations on GUI
+ *
+ * @author Siddhi Kasera, Sonal Madhok
+ **/
+public class ControllerMainMenu {
+
+
+    public void customerOrderPage(MouseEvent mouseEvent) {
+        //open fxml
+    }
+
+    public void storeOrdersPage(MouseEvent mouseEvent) {
+    }
+
+    public void orderCoffeePage(MouseEvent mouseEvent) {
+    }
+
+    public void orderDonutPage(MouseEvent mouseEvent) {
+    }
+}
